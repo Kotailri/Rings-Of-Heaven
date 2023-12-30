f71363405a1b1973c2f70c992dc6519b9c12e093
Kotailri <willc8810@gmail.com>
2023-12-29 22:48:23 -0800

    added damage vignette

---
+++ Assets/Scripts/Camera/VignetteTween.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VignetteTween : MonoBehaviour
+{
+    public Q_Vignette_Single vignette;
+
+    private float currentAlpha = 0.0f;
+    private float alphaIncrement = 0.01f;
+    private float alphaIncrementTime = 0.01f;
+
+    private bool isTweening = false;
+
+    public void SetVignetteDamage()
+    {
+        if (isTweening)
+        {
+            currentAlpha = 1;
+            return;
+        }
+
+        Color c = GetVignetteColor();
+        vignette.SetVignetteColour(new Color(c.r, c.g, c.b, 1));
+        currentAlpha = 1;
+        isTweening = true;
+
+        StartCoroutine(IncrementAlpha());
+    }
+
+    private IEnumerator IncrementAlpha()
+    {
+        currentAlpha -= alphaIncrement;
+
+        if (currentAlpha < 0)
+        {
+            isTweening = false;
+            yield return null;
+        }
+
+        Color c = GetVignetteColor();
+        vignette.SetVignetteColour(new Color(c.r, c.g, c.b, currentAlpha));
+
+        yield return new WaitForSeconds(alphaIncrementTime);
+        StartCoroutine(IncrementAlpha());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y)) 
+        {
+            SetVignetteDamage();
+        }
+    }
+
+    private Color GetVignetteColor()
+    {
+        return vignette.mainColor;
+    }
+}
